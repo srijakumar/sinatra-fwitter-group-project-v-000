15241ad0e2b8f8c7ccf02e09ebe637d7073906ab
Srija S <srijakumar7@gmail.com>
2018-11-21 12:14:20 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,13 @@ get '/login' do
 end
 
 post '/login' do
-  
+  user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect to "/tweets"
+   else
+     redirect to '/signup'
+   end
 end
 
 
