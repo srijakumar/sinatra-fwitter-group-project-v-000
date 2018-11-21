492b15960c06ab66a8e92aeb93c3d8d35cd7211b
Srija S <srijakumar7@gmail.com>
2018-11-21 19:00:19 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,15 +2,31 @@ require 'pry'
 class TweetsController < ApplicationController
 
 get '/tweets' do
-
 #  binding.pry
   if logged_in?
-
     @tweets = Tweet.all
     erb :'tweets/tweets'
   else
     redirect to '/login'
   end
-
 end
+
+get '/tweets/new' do
+  if logged_in?
+    erb :'tweets/create_tweet'
+  else
+    redirect to '/login'
+  end
+end
+
+post '/tweets' do
+  if params[:content] == ""
+    redirect '/tweets/new'
+  else
+    
+  end
+end
+
+
+
 end
